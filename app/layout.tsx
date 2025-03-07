e6ab3cb7b75ac06c4f64b26793a86c2d589946e9
ipinnova <ipinoovatechia@gmail.com>
2025-03-07 12:09:25 -0500

    implementacion pixel facebook

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,28 @@ export default function RootLayout({
             `,
           }}
         />
+
+        {/* Facebook Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', 'TU_ID_DE_PIXEL');  // Reemplaza TU_ID_DE_PIXEL con tu ID
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=TU_ID_DE_PIXEL&ev=PageView&noscript=1"/>
+        </noscript>
       </head>
       <body className={`${inter.className} antialiased`}>
         <LanguageProvider>
